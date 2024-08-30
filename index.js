b526d3a80c052fa8dde6bb0c3bca2edc5e6fef95
Joan Diaz <piasbcn@gmail.com>
2024-08-30 16:03:32 +0200

    Adapt backend to new DB

--- index.js
+++ index.js
@@ -21,14 +21,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -38,32 +33,31 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
+/*
 const generateId = () => {
     const maxId = notes.length > 0
       ? Math.max(...notes.map(n => Number(n.id)))
       : 0
     return String(maxId + 1)
   }
-  
+*/
+
 app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
   })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
   
   const PORT = process.env.PORT
   app.listen(PORT, () => {
