c2b9bb38b198e518563d0874b9be690ca057e7b1
Joan Diaz <piasbcn@gmail.com>
2024-08-30 15:46:30 +0200

    New model

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-]
+
+const notes = [];
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
   
 app.get('/api/notes', (request, response) => {    
+  Note.find({}).then(notes => {
     response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -76,7 +65,7 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
   })
   
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
